a7dbec4d943e693487f9d209104f227ea50ef0c7
Gemu <gemuandyou@163.com>
2017-03-22 22:56:55 +0800

    添加音乐盒

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,26 @@ import {Component} from '@angular/core';
     styleUrls: ['app/assets/styles/app.css', 'app/assets/styles/common.css']
 })
 export class AppComponent {
+
+    playMusic(ev, index) {
+        let audios = document.getElementsByClassName('music');
+        let target = ev.currentTarget;
+        if (target.className === 'playing') {
+            audios[index].pause();
+            audios[index].currentTime = audios[index].seekable.start(0);
+            target.removeAttribute('class');
+            return;
+        }
+        let lis = target.parentNode.children;
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i];
+            audios[i].pause();
+            audios[i].currentTime = audios[i].seekable.start(0);
+            li.removeAttribute('class');
+        }
+        target.setAttribute('class', 'playing');
+        audios[index].play();
+        console.log(audios[index]);
+    }
+
 }
