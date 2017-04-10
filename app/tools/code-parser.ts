bb7039088353e4b83be37d4473e94a6971e60697
gemu <suny@sf8cloud.com>
2017-04-10 08:41:54 +0800

    解析注释高亮编译失败问题修复

--- app/tools/code-parser.ts
+++ app/tools/code-parser.ts
@@ -81,15 +81,15 @@ export class CodeParser {
             codeTxt = codeTxt.replace(reg1, '<span style="font-weight: bold; color: #969696; font-size: 80%;">/*' + tmp.toString().substring(6, tmp.toString().length - 6) + '*/</span>')
         }
         // 第二种注释 // abc
-        let reg = new RegExp('//.*?\\n'); // 非贪婪匹配
-        for (let tmp = reg.exec(codeTxt); tmp != null;) {
-            codeTxt = codeTxt.replace(reg, '@anno1-' + tmp.toString().substring(2, tmp.toString().length - 2) + '-1onna@');
-            tmp = reg.exec(codeTxt);
+        let reg2 = new RegExp('//.*?\\n'); // 非贪婪匹配
+        for (let tmp = reg2.exec(codeTxt); tmp != null;) {
+            codeTxt = codeTxt.replace(reg2, '@anno1-' + tmp.toString().substring(2, tmp.toString().length - 2) + '-1onna@');
+            tmp = reg2.exec(codeTxt);
             console.log(tmp);
         }
-        let reg1 = new RegExp('@anno1-.*?-1onna@'); // 非贪婪匹配
-        for (let tmp = reg1.exec(codeTxt); tmp != null; tmp = reg1.exec(codeTxt)) {
-            codeTxt = codeTxt.replace(reg1, '<span style="font-weight: bold; color: #969696; font-size: 80%;">//' + tmp.toString().substring(7, tmp.toString().length - 7) + '</span>\n')
+        let reg3 = new RegExp('@anno1-.*?-1onna@'); // 非贪婪匹配
+        for (let tmp = reg3.exec(codeTxt); tmp != null; tmp = reg3.exec(codeTxt)) {
+            codeTxt = codeTxt.replace(reg3, '<span style="font-weight: bold; color: #969696; font-size: 80%;">//' + tmp.toString().substring(7, tmp.toString().length - 7) + '</span>\n')
         }
 
         return '<div style="font-family: Monaco,\'Lucida Console\',monospace;"><div style="font-family: fantasy;color: thistle; user-select: none;">' + renderParam + '</div>' +
