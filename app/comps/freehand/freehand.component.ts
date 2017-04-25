ca692dea69dd25160626c4dcb125c488b739c07a
gemu <suny@sf8cloud.com>
2017-04-25 14:05:56 +0800

    添加橡皮擦。 添加画笔和橡皮擦图标。

--- app/comps/freehand/freehand.component.ts
+++ app/comps/freehand/freehand.component.ts
@@ -10,22 +10,78 @@ import {Title} from "@angular/platform-browser";
 export class FreehandComponent implements AfterViewInit {
 
     imgsPreview: string[] = []; // 图片预览
+    isErase: boolean = false; // 画笔是否是橡皮檫
+    iconSize = 32; // 画笔图标长宽（px）
+    eraseSize = 20; // 橡皮擦图标长宽（px）
+    marginLeft = 200; // 画布左间隔长度（px）
+
+    @ViewChild('freehand') freehand;
 
     constructor(title: Title) {
         title.setTitle("鼠绘");
     }
 
     ngAfterViewInit() {
-        var script = document.createElement('script');
-        document.body.appendChild(script);
-        script.type = "text/javascript";
-        script.src = 'app/comps/freehand/freehand.js';
+        // var script = document.createElement('script');
+        // document.body.appendChild(script);
+        // script.type = "text/javascript";
+        // script.src = 'app/comps/freehand/freehand.js';
+
+        var cvsDom = document.getElementsByTagName('canvas')[0];
+        var cvsDiv = this.freehand.nativeElement;
+        var starting = false;
+        cvsDom.setAttribute('width', (cvsDiv.offsetWidth - this.marginLeft) + '');
+        cvsDom.setAttribute('height', (cvsDiv.offsetHeight) + '');
+        var ctx = cvsDom.getContext('2d');
+        cvsDom.onmouseover = () => {
+            if (this.isErase) {
+                cvsDom.style.cursor = 'url("app/assets/images/erase.png"), default';
+            } else {
+                cvsDom.style.cursor = 'url("app/assets/images/drawings.png"), default';
+            }
+        };
+        cvsDom.onmousedown = (e) => {
+            var target = e.srcElement;
+            var mX = e.pageX - target.clientLeft - this.marginLeft;
+            var mY = e.pageY - target.clientTop;
+            if (this.isErase) {
+                ctx.clearRect(mX + 2, mY + 2, this.eraseSize, this.eraseSize);
+            } else {
+                ctx.beginPath(); // 开始起点
+                ctx.moveTo(mX, mY + this.iconSize); // 开始移动
+                ctx.lineWidth = 1;
+            }
+            starting = true;
+        };
+        cvsDom.onmousemove = (e) => {
+            if (starting) {
+                var target = e.srcElement;
+                var lX = e.pageX - target.clientLeft - this.marginLeft;
+                var lY = e.pageY - target.clientTop;
+                if (this.isErase) {
+                    ctx.clearRect(lX + 2, lY + 2, this.eraseSize, this.eraseSize);
+                } else {
+                    ctx.lineTo(lX, lY + this.iconSize); // 移动
+                    ctx.strokeStyle = '#ECECEC';
+                    ctx.stroke(); // 画线
+                }
+            }
+        };
+        cvsDom.onmouseup = () => {
+            ctx.closePath(); // 结束点
+            starting = false;
+        };
+        cvsDom.onmouseout = () => {
+            ctx.closePath(); // 结束点
+            starting = false;
+        };
+
     }
 
     loadImg(e): void {
         let currEle = e.target;
         var cvsDom = document.getElementsByTagName('canvas')[0];
-        var ctx = cvsDom.getContext("2d");
+        var ctx = cvsDom.getContext('2d');
         ctx.clearRect(0, 0, cvsDom.offsetWidth, cvsDom.offsetHeight);
         ctx.drawImage(currEle, 0, 0, cvsDom.offsetWidth, cvsDom.offsetHeight);
     }
@@ -34,6 +90,16 @@ export class FreehandComponent implements AfterViewInit {
         var cvsDom = document.getElementsByTagName('canvas')[0];
         var imgBase64 = cvsDom.toDataURL('image/png');
         this.imgsPreview.push(imgBase64);
+    }
+
+    clearCanvas(): void {
+        var cvsDom = document.getElementsByTagName('canvas')[0];
+        var ctx = cvsDom.getContext('2d');
+        ctx.clearRect(0, 0, cvsDom.offsetWidth, cvsDom.offsetHeight);
+    }
+
+    eraserCanvas(): void {
+        this.isErase = !this.isErase;
     }
 
 }
