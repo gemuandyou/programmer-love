818531d8117c5d71b95d2b9008c81729ac04452a
gemuandyou <gemuandyou@163.com>
2017-05-16 18:07:47 +0800

    添加故事会模块页面-部分1

--- app/comps/stories/stories.component.ts
+++ app/comps/stories/stories.component.ts
@@ -8,7 +8,15 @@ import {Title} from "@angular/platform-browser";
     styleUrls: ['app/assets/styles/stories.css']
 })
 export class StoriesComponent {
+
+    isAutonym: boolean = true; // 是否是实名发布。否为匿名发布
+
     constructor(title: Title) {
         title.setTitle("故事会");
+    }
+
+    switchAutonym(isAutonym: boolean): void {
+        this.isAutonym = isAutonym;
+        // TODO 2017-5-16 17:50:30 刷新故事数据
     }
 }
--- app/comps/stories/stories.module.ts
+++ app/comps/stories/stories.module.ts
@@ -2,9 +2,10 @@
  * Created by Gemu on 2017/4/24.
  */
 import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
 import {StoriesComponent} from "./stories.component";
 @NgModule({
-    imports: [],
+    imports: [CommonModule],
     declarations: [
         StoriesComponent
     ]
