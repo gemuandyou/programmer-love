e2156a48f346349091c642f083832bd48a28b8a7
gemuandyou <gemuandyou@163.com>
2017-05-27 11:18:36 +0800

    update gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -5,64 +5,96 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const $ = require('gulp-load-plugins')();
 
-gulp.task('build-js', function(){
+gulp.task('build-js', function () {
     return gulp.src([
-        'app/**/*.js',
-        '*.js',
-        '!app/assets/**'
+        'app/**/*.js'
+    ]).pipe(uglify()).pipe(gulp.dest('dist/app'));
+});
+gulp.task('build-root-js', function () {
+    return gulp.src([
+        '*.js'
     ]).pipe(uglify()).pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-css', function(){
+gulp.task('build-css', function () {
     return gulp.src([
         'app/assets/styles/*.css'
+    ]).pipe($.minifyCss()).pipe(gulp.dest('dist/appt'));
+});
+
+gulp.task('build-root-css', function () {
+    return gulp.src([
+        '*.css'
     ]).pipe($.minifyCss()).pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-other', function(){
+gulp.task('build-html', function () {
+    return gulp.src([
+        'app/**/*.html',
+    ]).pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('build-root-html', function () {
+    return gulp.src([
+        '*.html'
+    ]).pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-module-other', function () {
+    return gulp.src([
+        //'node_modules/@angular/**',
+        //'node_modules/angular2-in-memory-web-api/**',
+        //'node_modules/bootstrap/**',
+        //'node_modules/core-js/**',
+        //'node_modules/font-awesome/**',
+        //'node_modules/http-proxy/**',
+        //'node_modules/phantom/**',
+        //'node_modules/reflect-metadata/**',
+        //'node_modules/rxjs/**',
+        //'node_modules/systemjs/**',
+        //'node_modules/zone.js/**',
+        //'node_modules/phantom/**',
+        //'node_modules/concurrently/**',
+        //'node_modules/lite-server/**',
+        //'node_modules/typescript/**',
+        //'node_modules/typings/**',
+        //'node_modules/core-js/**',
+        //'node_modules/core-js/**'
+        'node_modules/**/*',
+        '!node_modules/**/*.css'
+    ], {base: 'node_modules'}).pipe(gulp.dest('dist' + '/node_modules'));
+});
+
+gulp.task('build-module-css', function () {
+    return gulp.src([
+        'node_modules/**/*.css'
+    ], {base: 'node_modules'}).pipe($.minifyCss()).pipe(gulp.dest('dist' + '/node_modules'));
+});
+
+gulp.task('build-other', function () {
     return gulp.src([
         'app/**',
         '!app/**/*.js',
         '!app/**/*.ts',
         '!app/**/*.html',
         '!app/*.html'
-    ]).pipe(gulp.dest('dist'));
+    ]).pipe(gulp.dest('dist/app'));
 });
 
-gulp.task('build-html', function(){
-    return gulp.src([
-        'app/**/*.html',
-        'app/*.html',
-        '*.html'
-    ]).pipe(gulp.dest('dist'));
-});
-
-gulp.task('build-module', function(){
-    return gulp.src([
-        'node_modules/@angular/**',
-        'node_modules/angular2-in-memory-web-api/**',
-        'node_modules/bootstrap/**',
-        'node_modules/core-js/**',
-        'node_modules/font-awesome/**',
-        'node_modules/http-proxy/**',
-        'node_modules/phantom/**',
-        'node_modules/reflect-metadata/**',
-        'node_modules/rxjs/**',
-        'node_modules/systemjs/**',
-        'node_modules/zone.js/**',
-        'node_modules/phantom/**',
-        'node_modules/concurrently/**',
-        'node_modules/lite-server/**',
-        'node_modules/typescript/**',
-        'node_modules/typings/**',
-        'node_modules/core-js/**',
-        'node_modules/core-js/**'
-    ], {base: 'node_modules'}).pipe(gulp.dest('dist'+ '/node_modules'));
-});
-
-gulp.task('build-package', function(){
+gulp.task('build-package', function () {
     return gulp.src(['package.json'])
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['build-js','build-css','build-other','build-html', 'build-module', 'build-package']);
+gulp.task('build', [
+    'build-js',
+    'build-root-js',
+    'build-css',
+    'build-root-css',
+    'build-other',
+    'build-html',
+    'build-root-html',
+    'build-module-css',
+    'build-module-other',
+    'build-package'
+]);
