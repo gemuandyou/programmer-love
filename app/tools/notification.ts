eba3ba96434fccabd30889a5b28071344a74f034
gemuandyou <gemuandyou@163.com>
2017-06-06 16:21:40 +0800

    Notification提示优化

--- app/tools/notification.ts
+++ app/tools/notification.ts
@@ -3,14 +3,14 @@
  */
 export namespace Notify {
     export var success = (msg: string) => {
-
-        if (Notification.prototype.permission !== 'granted')
+        if (Notification.prototype.permission !== 'granted') {
             Notification.requestPermission(function(status){
-                if(Notification.prototype.permission !== status){
-                    Notification.prototype.permission = status;
-                }
+                //if(Notification.prototype.permission !== status){
+                //    Notification.prototype.permission = status;
+                //}
+                console.log(status);
             });
-        else {
+        } else {
             if (Notification.prototype.permission === 'granted') {
                 // If it's okay let's create a notification
                 var notification = new Notification('好消息', {body: msg, icon: 'app/assets/images/default_avatar.jpg'});
@@ -22,13 +22,11 @@ export namespace Notify {
     };
 
     export var error = (msg: string) => {
-        if (Notification.prototype.permission !== 'granted')
+        if (Notification.prototype.permission !== 'granted') {
             Notification.requestPermission(function(status){
-                if(Notification.prototype.permission !== status){
-                    Notification.prototype.permission = status;
-                }
+                console.log(status);
             });
-        else {
+        } else {
             if (Notification.prototype.permission === 'granted') {
                 // If it's okay let's create a notification
                 var notification = new Notification('坏消息', {body: msg, icon: 'app/assets/icons/default_avatar.jpg'});
@@ -40,13 +38,11 @@ export namespace Notify {
     };
 
     export var info = (msg: string) => {
-        if (Notification.prototype.permission !== 'granted')
+        if (Notification.prototype.permission !== 'granted') {
             Notification.requestPermission(function(status){
-                if(Notification.prototype.permission !== status){
-                    Notification.prototype.permission = status;
-                }
+                console.log(status);
             });
-        else {
+        } else {
             if (Notification.prototype.permission === 'granted') {
                 // If it's okay let's create a notification
                 var notification = new Notification('提示', {body: msg, icon: 'app/assets/icons/default_avatar.jpg'});
