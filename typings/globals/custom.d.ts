4b9998e65a63413eb2fb3e15f35321cfb7e30c37
Ge Mu <suny@sf8cloud.com>
2017-06-06 21:57:47 +0800

    Notification修改

--- typings/globals/custom.d.ts
+++ typings/globals/custom.d.ts
@@ -2,12 +2,6 @@
  * Created by gemu on 2/3/17.
  */
 
- declare var Notification: any;
-
- interface Notification {
-     permission: any;
- }
-
 interface Element {
     currentTime:any;
     seekable:any;
