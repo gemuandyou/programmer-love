bc4b38796efce2f020dd099f3d011cdc65efcfae
gemuandyou <gemuandyou@163.com>
2017-06-07 10:04:01 +0800

    d.ts文件提交

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/custom.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
-
-/// <reference path="globals/custom.d.ts" />
